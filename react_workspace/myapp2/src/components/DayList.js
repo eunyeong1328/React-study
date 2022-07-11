d62f4e654e2dc83e1dab27f40fcb38974be439e6
eunyoung56 <eunyeong1328@daum.net>
2022-07-11 16:52:20 +0900

    process json data

--- react_workspace/myapp2/src/components/DayList.js
+++ react_workspace/myapp2/src/components/DayList.js
@@ -1,12 +1,36 @@
-import data from "../database/days.json"
+//import data from "../database/days.json"
 import {Link} from "react-router-dom"
+import { useEffect, useState } from "react";
 
 const DayList = () => {
+    // const [count, setCount] = useState(0);
+    // const onClick = () => {
+    //     setCount(count + 1);
+    // } 
+    // useEffect(() => {
+    //     console.log(`count --> ${count}`);
+    // },[]);
+    const [days,setDays] = useState([]);
+
+    //처음 렌더링될때, 한번 실행(useEffect((),[]))
+    useEffect(() => {
+        console.log(`json서버로 부터 (days 데이터 읽어옴)`);
+        fetch("http://localhost:3001/days") //값을 가져옴
+        .then((res)=>{
+            return res.json();
+        })
+        .then((data)=>{
+            setDays(data);
+            console.log(`DayList() --> ${data}`);
+        })
+    },[]);
+
     return(
         <div>
+            {/* <button onClick = {onClick}>{count}</button> */}
             <ul className="list_day">
                 {
-                 data.days.map(
+                 days.map(
                     (day) => {
                         return (
                             <li key = {day.id}>
