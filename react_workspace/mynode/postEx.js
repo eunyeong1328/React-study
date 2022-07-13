f37f2d9d9caacea32a0bf71dca47aa12d741258f
eunyoung56 <eunyeong1328@daum.net>
2022-07-13 16:46:18 +0900

    Two types of data processing in get,post method transmission

--- react_workspace/mynode/postEx.js
+++ react_workspace/mynode/postEx.js
@@ -11,9 +11,22 @@ let server = http.createServer(app).listen(app.get('port'),()=>{
 //특정 폴더를 url로 접근하기 위한 설정(미들웨어)
 let static = require('serve-static');
 let path = require('path'); //현재 디렉토리 정보
+const { urlencoded } = require('express');
 
 let pathName = path.join(__dirname, 'public'); //__dirname 현재 디렉토리 정보
 console.log('pathName :' + pathName);
 
 app.use('/public', static(pathName)); //localhost:3000/public/으로 public 폴더 접근가능
 app.use(static(pathName));//loaclhost:3000/ 로 접근가능
+
+app.use(express.urlencoded());//post : application/x-www-form-urlencoded
+app.use(express.json());//post : application//json
+
+app.use((req,res) =>{
+    console.log('post 방식 데이터 처리');
+    let id = req.body.id || req.query.id; //get방식 || post 방식
+    let password = req.body.password || req.query.password; //get방식 || post 방식
+    console.log('id: ' + id + ', password : ' + password);
+
+     res.send('id: ' + id + ', password : ' + password);
+})
