4461839356ed6fbe8c4fe9a0801e4808c64e4e7b
eunyoung <72347107+eunyoung56@users.noreply.github.com>
2022-07-18 12:14:15 +0900

    고객관리 page 추가

---
+++ react_workspace/myapp3/client/src/components/Customer.js
@@ -0,0 +1,45 @@
+const Customer = (props) => {
+    const imgStyle = {
+        width: "100px",
+        height: "100px",
+    }
+    return (
+        <tr>
+            <td>{props.id}</td>
+            <td><img src={props.image} style = {imgStyle}/></td>
+            <td>{props.name}</td>
+            <td>{props.birthday}</td>
+            <td>{props.job}</td>
+        </tr>
+        // <div>
+        //     <CustomerProfile 
+        //         id={props.id}
+        //         name={props.name}
+        //         image={props.image} />
+        //     <CustomerInfo
+        //         birthday={props.birthday}
+        //         job={props.job} />
+        // </div>
+    )
+}
+
+const CustomerProfile = (props) => {
+    return (
+        <div>
+            <img src={props.image} alt="profile" />
+            <p>id: {props.id}</p>
+            <p>name: {props.name}</p>
+        </div>
+    )
+}
+
+const CustomerInfo = (props) => {
+    return (
+        <div>
+            <p>birthday: {props.birthday}</p>
+            <p>job: {props.job}</p>
+        </div>
+    )
+}
+
+export default Customer;
