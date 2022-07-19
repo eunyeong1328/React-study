489d3818ec62a4dc4c2bb755f23d4ab3e4c45ea6
eunyoung <72347107+eunyoung56@users.noreply.github.com>
2022-07-19 14:38:18 +0900

    implement mongodb connection

--- react_workspace/mynode/mongodbEx.js
+++ react_workspace/mynode/mongodbEx.js
@@ -6,6 +6,7 @@ app.set('port',3000);
 
 let server = http.createServer(app).listen(app.get('port'),()=>{
     console.log('express 이용 서버 실행중..');
+    connectDB(); //호출을 해야한다.
 })
 
 //특정 폴더를 url로 접근하기 위한 설정(미들웨어)
@@ -26,3 +27,19 @@ app.use(express.json());//post : application//json
 let router = express.Router();
 //중요. '/' 입력되면, router객체로 연결
 app.use('/',router);
+
+//mongoDB 연결
+let mongoClient = require('mongodb').MongoClient;
+let database = null;
+
+const connectDB  = () => {
+    let dbUrl = 'mongodb://localhost:27017/nodeDB'
+    mongoClient.connect(dbUrl, (err,db) => {
+        if(err){
+            console.log('몽고디비 연결 실패!!');
+            return;
+        }
+        database = db.db('nodeDB'); //local 디비에 접근 전역변수로, 제대로 연결이 되어있는지 확인
+        console.log('몽고디비 연결 성공!!');
+    })
+}
